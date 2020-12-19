7bebf7091be480a8a887c3bd308bddebfcd9c2b8
Jaap <jaap@mrproper.dev>
2020-12-19 11:49:32 +0100

    typo

--- commands/ascii.js
+++ commands/ascii.js
@@ -14,7 +14,7 @@ module.exports = {
       (err, converted) => {
         if (err) return bot.sendNotification(`Invalid image! Please use .ascii <imgurl>`, 'error', msg);
         if (converted.length >= 2000 - 6)
-          return msg.channel.send(`Sorry, couldn't progress your image. The ascii result is longer then 2000 characters :(`);
+          return msg.channel.send(`Sorry, couldn't progress your image. The ascii result is longer than 2000 characters :(`);
         msg.channel.send('```' + converted + '```').catch(console.error);
       }
     );
